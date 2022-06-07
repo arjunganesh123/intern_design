d3d94a30fa35baa58e7efb57494ee7d850235aa2
arjunganesh123 <arjunganeshgonibeedu44@gmail.com>
2022-06-07 23:45:16 +0530

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: const HomePage(),
+      home: const UserAuthPage(),
       routes: {
         HomePage.id:(context)=>const HomePage(),
         UserAuthPage.id:(context)=>const UserAuthPage(),
